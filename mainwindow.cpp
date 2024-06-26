8597c0c0484ecfacaac519a2f7fe7bee4f7e2619
Damian Skórka <unearth179@gmail.com>
2024-06-27 01:13:17 +0200

    main refactor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,18 +1,24 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <QTimer>
-#include <QDateTime>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    QTimer* timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(showTime()));
-    timer->start();
+    clock = new Clock(this);
+    connect(clock, &Clock::timeChanged, this, &MainWindow::updateClock);
+
+    alarm = new Alarm(this);
+    connect(alarm, &Alarm::alarmRinging, this, &MainWindow::handleAlarm);
+
+    timer = new Timer(this);
+    connect(timer, &Timer::timerUpdated, this, &MainWindow::updateTimer);
+    connect(timer, &Timer::timerFinished, this, &MainWindow::timerFinished);
+
+    connect(ui->setAlarmButton, &QPushButton::clicked, this, &MainWindow::setAlarm);
+    connect(ui->startTimerButton, &QPushButton::clicked, this, &MainWindow::startTimer);
 }
 
 MainWindow::~MainWindow()
@@ -20,20 +26,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::showTime()
+void MainWindow::updateClock(const QString &time)
 {
-    QTime time = QTime::currentTime();
-    QString time_text = time.toString("hh : mm : ss");
-
-    time_text[8] = (time.second() % 2 == 0 ? ' ' : ':');
-    /*
-    if ((time.second() % 2 == 0))
-    {
-        time_text[8] = ' ';
-    }
-    */
-    ui->Digital_clock->setText(time_text);
+    ui->Digital_clock->setText(time);
 }
 
+void MainWindow::handleAlarm()
+{
+    ui->alarmStatusLabel->setText("Alarm ringing!");
+    // Tutaj możesz dodać dodatkowe działania, np. odtwarzanie dźwięku
+}
 
-// just for development commit
+void MainWindow::updateTimer(const QString &time)
+{
+    ui->timerStatusLabel->setText(time);
+}
+
+void MainWindow::timerFinished()
+{
+    ui->timerStatusLabel->setText("Time's up!");
+    // Tutaj możesz dodać dodatkowe działania, np. odtwarzanie dźwięku
+}
+
+void MainWindow::setAlarm()
+{
+    alarm->setAlarmTime(ui->alarmTimeEdit->time());
+    ui->alarmStatusLabel->setText("Alarm set for " + alarm->getAlarmTime().toString("hh:mm:ss"));
+}
+
+void MainWindow::startTimer()
+{
+    int minutes = ui->timerMinutesSpinBox->value();
+    int seconds = ui->timerSecondsSpinBox->value();
+    timer->startTimer(minutes, seconds);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "clock.h"
+#include "alarm.h"
+#include "timer.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,10 +21,18 @@ public:
     ~MainWindow();
 
 private slots:
-    void showTime();
+    void updateClock(const QString &time);
+    void handleAlarm();
+    void updateTimer(const QString &time);
+    void timerFinished();
+    void setAlarm();
+    void startTimer();
 
 private:
     Ui::MainWindow *ui;
-
+    Clock *clock;
+    Alarm *alarm;
+    Timer *timer;
 };
+
 #endif // MAINWINDOW_H
